d5daa0970b255cc80df331a9f3c8529d00a8759b
Ross Kelso <rosskelso@btinternet.com>
2018-07-07 18:34:28 +0100

    Added dynamic web socket address

--- static/model.js
+++ static/model.js
@@ -126,7 +126,15 @@ var model = (function() {
      * Initialise the web socket connection.
      */
     function init(gameId) {
-        ws = new WebSocket("ws://" + window.location.hostname + "/" + gameId);
+        var loc = window.location, new_uri;
+        if (loc.protocol === "https:") {
+            new_uri = "wss:";
+        } else {
+            new_uri = "ws:";
+        }
+        new_uri += "//" + loc.host;
+        new_uri += loc.pathname + gameId;
+        ws = new WebSocket(new_uri);
         //ws = new WebSocket("ws://echo.websocket.org");
         ws.addEventListener("open", function () {
             console.log("socket opended");
